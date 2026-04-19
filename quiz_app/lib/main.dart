11fc43f8999b749ad5a63bd8dcc29ea3cc03af0c
unknown <chirantannt19@gmail.com>
2026-04-19 21:08:00 +0530

    remove quiz_app files

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -22,7 +22,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
